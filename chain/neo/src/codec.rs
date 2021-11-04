1352152a3245b08b4c9b6c45850abb05a960a51c
Tobeyw <tobey1024@126.com>
2021-11-04 16:57:27 +0800

    add codec

--- chain/neo/src/codec.rs
+++ chain/neo/src/codec.rs
@@ -1,1 +1,24 @@
- 
+#[path = "protobuf/sf.neo.codec.v1.rs"]
+mod pbcodec;
+
+use graph::{
+    blockchain::Block as Blockchainblock,
+    blockchain::BlockPtr,
+    prelude::{hex, web3::types::H256, BlockNumber},
+};
+use std::convert::TryFrom;
+use std::fmt::LowerHex;
+
+pub use pbcodec::*;
+
+impl From<&CryptoHash> for H256 {
+    fn from(input: &CryptoHash) -> Self {
+        H256::from_slice(&input.hash)
+    }
+}
+
+impl LowerHex for &CryptoHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&hex::encode(&self.hash))
+    }
+}
